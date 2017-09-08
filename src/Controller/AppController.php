4ffb2b58e99526b0d38cabf58566c60827a491e3
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2017-09-08 17:52:56 -0300

    Initial commit

---
+++ src/Controller/AppController.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     0.2.9
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Controller;
+
+use Cake\Controller\Controller;
+use Cake\Event\Event;
+use Cake\I18n\Time;
+use Cake\Network\Exception\NotAcceptableException;
+use PHPMailer\PHPMailer\PHPMailer;
+
+/**
+ * Application Controller
+ *
+ * Add your application-wide methods in the class below, your controllers
+ * will inherit them.
+ *
+ * @link https://book.cakephp.org/3.0/en/controllers.html#the-app-controller
+ */
+class AppController extends Controller
+{
+
+    /**
+     * Initialization hook method.
+     *
+     * Use this method to add common initialization code like loading components.
+     *
+     * e.g. `$this->loadComponent('Security');`
+     *
+     * @return void
+     */
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->loadComponent('RequestHandler');
+        $this->loadComponent('Flash');
+
+        /*
+         * Enable the following components for recommended CakePHP security settings.
+         * see https://book.cakephp.org/3.0/en/controllers/components/security.html
+         */
+        //$this->loadComponent('Security');
+        //$this->loadComponent('Csrf');
+    }
+
+    /**
+     * Before render callback.
+     *
+     * @param \Cake\Event\Event $event The beforeRender event.
+     * @return \Cake\Http\Response|null|void
+     */
+    public function beforeRender(Event $event)
+    {
+        // Note: These defaults are just to get started quickly with development
+        // and should not be used in production. You should instead set "_serialize"
+        // in each action as required.
+        if (!array_key_exists('_serialize', $this->viewVars) &&
+            in_array($this->response->type(), ['application/json', 'application/xml'])
+        ) {
+            $this->set('_serialize', true);
+        }
+    }
+
+	public function enviarEmail($dados)
+	{
+		if (!isset($dados["corpo_email"]))
+			throw new NotAcceptableException();
+
+		$dadosPadroes = [
+			"email_remetente" => "smtp@tgroups.com.br",
+			"nome_remetente" => "Salamitos Rir Remetente",
+			"email_destino" => "paulo.salvatore@tgroups.com.br",
+			"nome_destino" => "Paulo Salvatore",
+			"assunto" => "Assunto"
+		];
+
+		foreach ($dadosPadroes as $chave => $valor)
+			if (!isset($dados[$chave]))
+				$dados[$chave] = $valor;
+
+		$dados["corpo_email"] .= 'Mensagem enviada em ' . Time::now() . '.';
+
+		$mail = new PHPMailer();
+
+		//$mail->SMTPDebug = 2;
+
+		$mail->isSMTP();
+		$mail->Host = "mail.tgroups.com.br";
+		$mail->SMTPAuth = true;
+		$mail->Username = "smtp@tgroups.com.br";
+		$mail->Password = "nova@2017";
+		$mail->SMTPAutoTLS = false;
+		$mail->Port = 587;
+		$mail->CharSet = "UTF-8";
+
+		$mail->setFrom($dados["email_remetente"], $dados["nome_remetente"]);
+		$mail->addReplyTo($dados["email_remetente"], $dados["nome_remetente"]);
+		$mail->addAddress($dados["email_destino"], $dados["nome_destino"]);
+
+		$mail->isHTML(true);
+
+		$mail->Subject = $dados["assunto"];
+
+		$mail->Body = $dados["corpo_email"];
+
+		$mail->addAttachment($dados["video"]);
+
+		$enviarEmail = $mail->send();
+
+		$dados["enviado"] = $enviarEmail;
+
+		return [
+			"mail" => $mail,
+			"dados" => $dados
+		];
+	}
+}
---
+++ src/Controller/PagesController.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     0.2.9
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Network\Exception\ForbiddenException;
+use Cake\Network\Exception\NotFoundException;
+use Cake\View\Exception\MissingTemplateException;
+
+set_time_limit(60 * 60);
+
+/**
+ * Static content controller
+ *
+ * This controller will render views from Template/Pages/
+ *
+ * @link https://book.cakephp.org/3.0/en/controllers/pages-controller.html
+ */
+class PagesController extends AppController
+{
+
+    /**
+     * Displays a view
+     *
+     * @param array ...$path Path segments.
+     * @return \Cake\Http\Response|null
+     * @throws \Cake\Network\Exception\ForbiddenException When a directory traversal attempt.
+     * @throws \Cake\Network\Exception\NotFoundException When the view file could not
+     *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
+     */
+    public function display(...$path)
+    {
+        $count = count($path);
+        if (!$count) {
+            return $this->redirect('/');
+        }
+        if (in_array('..', $path, true) || in_array('.', $path, true)) {
+            throw new ForbiddenException();
+        }
+        $page = $subpage = null;
+
+        if (!empty($path[0])) {
+            $page = $path[0];
+        }
+        if (!empty($path[1])) {
+            $subpage = $path[1];
+        }
+
+        if ($page == "home")
+		{
+			$diretorioVideosEnviar = "videos_enviar/";
+			$diretorioVideosEnviados = "videos_enviados/";
+			$videosEnviar = scandir($diretorioVideosEnviar);
+
+			$emailEnviado = false;
+			$videoEncontrado = false;
+
+			foreach ($videosEnviar as $json)
+			{
+				if ($json != "." && $json != ".." && strpos($json, ".json") !== false)
+				{
+					$video = str_replace(".json", ".mp4", $json);
+
+					$destino = json_decode(file_get_contents($diretorioVideosEnviar . $json), true);
+
+					$dados = [
+						"corpo_email" => "Corpo do E-mail<br>",
+						"video" => $diretorioVideosEnviar . $video,
+						"email_destino" => $destino["email"],
+						"nome_destino" => $destino["username"]
+					];
+
+					$email = $this->enviarEmail($dados);
+
+					if ($email["dados"]["enviado"])
+					{
+						rename($diretorioVideosEnviar . $json, $diretorioVideosEnviados . $json);
+						rename($diretorioVideosEnviar . $video, $diretorioVideosEnviados . $video);
+
+						echo "Vídeo " . $video . " enviado para " . $destino["username"] . " (" . $destino["email"] . ").";
+
+						$emailEnviado = true;
+					}
+
+					$videoEncontrado = true;
+
+					break;
+				}
+			}
+
+			if (!$videoEncontrado)
+				echo 'Nenhum vídeo foi encontrado.';
+			elseif (!$emailEnviado)
+				echo 'Um vídeo foi encontrado mas o e-mail não foi enviado';
+
+			echo'<meta http-equiv="refresh" content="1">';
+		}
+
+        $this->set(compact('page', 'subpage'));
+
+        try {
+            $this->render(implode('/', $path));
+        } catch (MissingTemplateException $exception) {
+            if (Configure::read('debug')) {
+                throw $exception;
+            }
+            throw new NotFoundException();
+        }
+    }
+}
